a1d94b9ebbb82174a5e4fbcf72fc7ec789df4f0d
Becca-90 <z3378134@ad.unsw.edu.au>
2022-02-09 16:38:08 +1100

    some self-thinning plots with trait value comparisons, tidied some things up

--- R/grow_tidy_expand_totals function.R
+++ R/grow_tidy_expand_totals function.R
@@ -1,4 +1,4 @@
-grow_tidy_expand_totals <- function(oderel=1e-4, odeabs=1e-4, scheps=0.005, rec_decay=0, traits) {
+grow_tidy_expand_totals <- function(oderel=1e-4, odeabs=1e-4, scheps=0.0001, rec_decay=5, traits=trait_matrix(0.625, "lma"), B_lf1 = 1, narea = 0.00187) {
   
   
   control = scm_base_control()
@@ -8,13 +8,16 @@ grow_tidy_expand_totals <- function(oderel=1e-4, odeabs=1e-4, scheps=0.005, rec_
   control$schedule_eps <- scheps
   
   p = base_params(rec_decay = rec_decay)
-  patch <- run_mypatch_edit(p0 = p, ctrl = control, traits = traits)
+  patch <- run_mypatch_edit(p0 = p, ctrl = control, traits = traits, B_lf1 = B_lf1, narea = narea)
   patch_tidy <- tidy_patch(patch)
   patch_expand <- FF16_expand_state(patch_tidy)
   
   patch_expand$species %>% 
   integrate_over_size_distribution() %>%
     mutate(
-      area_stem_av = area_stem / individuals
+      area_stem_av = area_stem / individuals,
+      height_av = height / individuals,
+      stem_diameter_av = (2 * sqrt (area_stem_av / pi))
       )
+  
 }
--- R/run_mypatch_edit function.R
+++ R/run_mypatch_edit function.R
@@ -1,13 +1,14 @@
 run_mypatch_edit <- function(
   traits =  trait_matrix(c(0.07), c("lma")),
   B_lf1 = 1,
+  narea = 0.00187,
   birth_rate = 200,
   p0 = base_params(),
   ctrl=scm_base_control(),
   latitude = 28.182
 ) {
   
-  hyper_par_fn = make_FF16_hyperpar(B_lf1 = B_lf1, latitude = latitude)
+  hyper_par_fn = make_FF16_hyperpar(B_lf1 = B_lf1, latitude = latitude, narea = narea)
   p1 <- expand_parameters(traits, p0, hyper_par_fn, mutant = FALSE)
   p1$birth_rate = birth_rate
   results <- build_schedule(p1, ctrl = ctrl)
