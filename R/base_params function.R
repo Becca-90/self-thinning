bc43ea10949b1b80168b0575150a643b7fae5c83
Daniel Falster <daniel.falster@unsw.edu.au>
2022-02-21 06:55:23 +1100

    Suggested refinements

--- R/base_params function.R
+++ R/base_params function.R
@@ -1,11 +1,8 @@
-base_params <- function(rec_decay = 0, lma = 0.1, rho=500, hmat=25) {
+base_params <- function(rec_decay = 0) {
   
   p0 <- scm_base_parameters("FF16")
   
-  p0$strategy_default$hmat <- 25
-  p0$strategy_default$lma <-  lma
-  p0$strategy_default$rho <-  rho
-  p0$strategy_default$a_l1 <- 2.17
+  p0$strategy_default$a_l1 <- 3
   p0$strategy_default$a_l2 <- 0.5
   
   p0$strategy_default$recruitment_decay <- rec_decay
--- R/run_mypatch_edit function.R
+++ R/run_mypatch_edit function.R
@@ -1,14 +1,14 @@
 run_mypatch_edit <- function(
-  traits =  trait_matrix(c(0.07), c("lma")),
-  B_lf1 = 1,
-  narea = 0.00187,
+  traits =  trait_matrix(c(0.24), c("lma")),
   birth_rate = 200,
   p0 = base_params(),
   ctrl=scm_base_control(),
-  latitude = 28.182
+  latitude = 28.182,
+  ...
 ) {
   
-  hyper_par_fn = make_FF16_hyperpar(B_lf1 = B_lf1, latitude = latitude, narea = narea)
+  hyper_par_fn = make_FF16_hyperpar(...)
+  
   p1 <- expand_parameters(traits, p0, hyper_par_fn, mutant = FALSE)
   p1$birth_rate = birth_rate
   results <- build_schedule(p1, ctrl = ctrl)
